fe1025efbccbe08eb1874ee538118ad5195e4d7b
Pooja <pooja.isabelle@gmail.com>
2020-10-16 12:56:09 +1100

    Read in the Census Data CSV

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,9 @@
-// @TODO: YOUR CODE HERE!
+// Load data from hours-of-tv-watched.csv
+d3.csv("./data.csv").then(function(censusData) {
+
+    console.log(censusData);
+  
+}).catch(function(error) {
+    console.log(error);
+});
+  
